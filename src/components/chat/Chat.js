7b3aea2546cb06c9bd55414bcbc29a225d200c20
h2o-Noob <arindamraina7743@gmail.com>
2022-01-31 21:26:32 +0530

    added chatengine.io

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,16 +1,17 @@
 import React, { useRef, useState, useEffect } from "react"
-
 import axios from 'axios'
 import { useHistory } from "react-router-dom"
 import { ChatEngine } from 'react-chat-engine'
-import { useAuth } from "../contexts/ChatContext"
+import { ChatEngineWrapper, ChatSocket, ChatFeed } from 'react-chat-engine'
+import { useAuth } from "../contexts/AuthContext"
 import { auth } from "../../firebase"
 
 export default function Chats() {
-//   const didMountRef = useRef(false)
-//   const [ loading, setLoading ] = useState(true)
-//   const { user } = useAuth()
-//   const history = useHistory()
+  const didMountRef = useRef(false)
+  const [ loading, setLoading ] = useState(true)
+  const { user } = useAuth()
+  const history = useHistory()
+  console.log(user)
 
 //   async function handleLogout() {
 //     await auth.signOut()
@@ -23,51 +24,49 @@ export default function Chats() {
 //     return new File([data], "test.jpg", { type: 'image/jpeg' });
 //   }
 
-//   useEffect(() => {
-//     if (!didMountRef.current) {
-//       didMountRef.current = true
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
 
-//       if (!user || user === null) {
-//         history.push("/")
-//         return
-//       }
+      if (!user || user === null) {
+        history.push("/")
+        return
+      }
       
-//       axios.get(
-//         'https://api.chatengine.io/users/me/',
-//         { headers: { 
-//           "project-id": "0e48d3a8-952a-45be-acd5-328896731174",
-//           "user-name": user.email,
-//           "user-secret": user.uid
-//         }}
-//       )
+      axios.get(
+        'https://api.chatengine.io/users/me/',
+        { headers: { 
+          "project-id": "0e48d3a8-952a-45be-acd5-328896731174",
+          "user-name": "John Appleseed",
+          "user-secret": "123"
+        }}
+      )
 
-//       .then(() => setLoading(false))
+      .then(() => setLoading(false))
 
-//       .catch(e => {
-//         let formdata = new FormData()
-//         formdata.append('email', user.email)
-//         formdata.append('username', user.email)
-//         formdata.append('secret', user.uid)
+      .catch(e => {
+        let formdata = new FormData()
+        formdata.append('email', user.email)
+        formdata.append('username', user.displayName)
+        formdata.append('secret', user.uid)
 
 //         getFile(user.photoURL)
 //         .then(avatar => {
 //           formdata.append('avatar', avatar, avatar.name)
 
-//           axios.post(
-//             'https://api.chatengine.io/users/',
-//             formdata,
-//             { headers: { "private-key": "97708d08-846b-42c0-a2b9-8898ad8b9352" }}
-//           )
-//           .then(() => setLoading(false))
-//           .catch(e => console.log('e', e.response))
-//         })
-//       })
-
-//     }
-//   }, [user, history])
+          axios.post(
+            'https://api.chatengine.io/users/',
+            formdata,
+            { headers: { "private-key": "97708d08-846b-42c0-a2b9-8898ad8b9352" }}
+          )
+          .then(() => setLoading(false))
+          .catch(e => console.log('e', e.response))
+        })
+    }
+  }, [user, history])
   
 
-//   if (!user || loading) return <div />
+  // if (!user || loading) return <div />
 
   return (
     // <div className='chats-page'>
@@ -81,19 +80,24 @@ export default function Chats() {
     //     </div>
     //   </div>
 
-    //   <ChatEngine 
-    //     height='calc(90vh)'
-    //     projectID= {"0e48d3a8-952a-45be-acd5-328896731174"}
-    //     userName={user.email}
-    //     userSecret={user.uid}
-    //   />
+      <ChatEngine 
+        height='calc(100vh)'
+        projectID= {"0e48d3a8-952a-45be-acd5-328896731174"}
+        userName={user.displayName}
+        userSecret={user.uid}
+      />
     // </div>
     
-    <ChatEngine
-			projectID='0e48d3a8-952a-45be-acd5-328896731174'
-			userName='adam'
-			userSecret='pass1234'
-		/>
+    // <ChatEngineWrapper>
+    //         <ChatSocket 
+    //             projectID='0e48d3a8-952a-45be-acd5-328896731174'
+    //             chatID='93394'
+    //             chatAccessKey='ca-a6493a61-8b2d-41c5-96d8-cebeeeae2218'
+    //             senderUsername='John Appleseed'
+    //         />
+
+    //         <ChatFeed activeChat='123' /> 
+    //     </ChatEngineWrapper>
   )
 
 }
